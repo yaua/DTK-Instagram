e984c484462fccc3d0d9f787da6fa3fe6808290a
yaua <phungngoctho@gmail.com>
2020-12-10 13:15:27 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,39 +14,30 @@ int main(int argc, char *argv[])
 {
     DApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
+    a.setApplicationName(DEFAULT_TITLE);
+    a.setProductName(DEFAULT_TITLE);
     DAboutDialog *dialog = new DAboutDialog;
     a.setAboutDialog(dialog);
-    dialog->setProductIcon(QIcon(":/images/logo.svg"));
+    dialog->setProductName(QString("<span>%1</span>").arg(DEFAULT_TITLE));
+    dialog->setProductIcon(QIcon(":/images/instagram.svg"));
     QString yDefaultDesc = QString("<span style=' font-size:8pt; font-weight:600;'>This is the simple app with dtk lib and WebApp Runtime. Customize by yaua</span>");
     dialog->setDescription(yDefaultDesc);
-    dialog->setVersion(DApplication::buildVersion(QString("%1 %2").arg(QObject::tr("Version")).arg("1.1")));
+    dialog->setVersion(DApplication::buildVersion(QString("%1 %2").arg(QObject::tr("Version")).arg("1.2.0")));
     dialog->setLicense(QObject::tr("This program is open source under GPLv3"));
-
     QString yTitle = DEFAULT_TITLE;
     QString yUrl = DEFAULT_URL;
     int width = DEFAULT_WIDTH;
     int height = DEFAULT_HEIGHT;
     bool tray = true;
-    bool fullScreen = false;
-    bool fixSize = false;
     bool hideButtons = false;
     QString yIcon = DEFAULT_ICON;
-    QString yDesc = DEFAULT_DESC;
-    if(fixSize)
-    {
-        fullScreen = false;
-    }
     a.setQuitOnLastWindowClosed(!tray);
-    MainWindow w(yTitle, yUrl, width, height, tray, fullScreen, fixSize, hideButtons);
+    MainWindow w(yTitle, yUrl, width, height, tray, hideButtons);
     if (!yIcon.isEmpty())
     {
         dialog->setWindowIcon(QIcon(yIcon));
         dialog->setProductIcon(QIcon(yIcon));
         w.setIcon(yIcon);
-    }
-    if (!yDesc.isEmpty())
-    {
-        dialog->setDescription(yDesc);
     }
     w.show();
     return a.exec();
