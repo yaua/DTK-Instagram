7bdbf7622c83da997310ca2b4dfebc656a9a9e42
Gray <phungngoctho@gmail.com>
2020-12-11 14:37:26 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,42 +3,33 @@
 #include <DWidgetUtil>
 #include <DAboutDialog>
 #include <DMainWindow>
-#include <QTranslator>
 #include <QFileInfo>
 #include <QSettings>
-#include "main.h"
 
 DWIDGET_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
     DApplication a(argc, argv);
+    DAboutDialog dialog;
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
-    a.setApplicationName(DEFAULT_TITLE);
-    a.setProductName(DEFAULT_TITLE);
-    DAboutDialog *dialog = new DAboutDialog;
-    a.setAboutDialog(dialog);
-    dialog->setProductName(QString("<span>%1</span>").arg(DEFAULT_TITLE));
-    dialog->setProductIcon(QIcon(":/images/instagram.svg"));
+    a.setApplicationName("Instagram");
+    a.setProductName("Instagram");
+    a.setAboutDialog(&dialog);
+    dialog.setProductName(QString("<span>%1</span>").arg("Instagram"));
+    dialog.setProductIcon(QIcon(":/images/instagram.svg"));
     QString yDefaultDesc = QString("<span style=' font-size:8pt; font-weight:600;'>This is the simple app with dtk lib and WebApp Runtime. Customize by yaua</span>");
-    dialog->setDescription(yDefaultDesc);
-    dialog->setVersion(DApplication::buildVersion(QString("%1 %2").arg(QObject::tr("Version")).arg("1.2.0")));
-    dialog->setLicense(QObject::tr("This program is open source under GPLv3"));
-    QString yTitle = DEFAULT_TITLE;
-    QString yUrl = DEFAULT_URL;
-    int width = DEFAULT_WIDTH;
-    int height = DEFAULT_HEIGHT;
+    dialog.setDescription(yDefaultDesc);
+    dialog.setVersion(DApplication::buildVersion(QString("%1 %2").arg(QObject::tr("Version")).arg("1.2.1")));
+    dialog.setLicense((QObject::tr("This program is open source under GPLv3")));
+    QString yTitle = "Instagram";
+    QString yUrl = "https://www.instagram.com/";
+    int width = 700;
+    int height = 800;
     bool tray = true;
     bool hideButtons = false;
-    QString yIcon = DEFAULT_ICON;
     a.setQuitOnLastWindowClosed(!tray);
     MainWindow w(yTitle, yUrl, width, height, tray, hideButtons);
-    if (!yIcon.isEmpty())
-    {
-        dialog->setWindowIcon(QIcon(yIcon));
-        dialog->setProductIcon(QIcon(yIcon));
-        w.setIcon(yIcon);
-    }
     w.show();
     return a.exec();
 }
